a84628edf9ff741709b2a8e22e0d37f6d462a38f
Ravishan Balasooriya <ravimuay@gmail.com>
2021-07-29 19:32:01 +0530

    Added counter jsx

---
+++ src/components/counter.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    
+    render() { 
+
+        return( 
+        <div>
+            <h1>Hello World</h1>
+            <button>Incrment</button>
+
+        </div>
+        );
+    }
+}
+ 
+export default Counter ;
